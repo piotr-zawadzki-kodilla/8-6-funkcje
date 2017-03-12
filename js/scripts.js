41f48e204186adb76174986bad35da8ca94725b7
zaramid <piotr@zawadzki.net>
2017-03-12 14:06:20 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+function getTriangleArea(a, h) {
+
+	if (a > 0 && h > 0) {
+		return a*h/2;
+	}
+	else {
+		return "Nieprawidłowe dane";
+	}
+}
+
+console.log(getTriangleArea(10,6));
+
+var triangleArea1 = getTriangleArea(20,0);
+var triangleArea2 = getTriangleArea(7,9);
+var triangleArea3 = getTriangleArea(250,113);
+
+console.log(triangleArea1);
+console.log(triangleArea2);
+console.log(triangleArea3);
